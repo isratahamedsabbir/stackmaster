aab083d2b72c5dcce51065926e5e0122dd3da2ce
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-04-14 11:01:52 +0600

    u

--- resources/views/backend/partials/notification.blade.php
+++ resources/views/backend/partials/notification.blade.php
@@ -2,8 +2,14 @@
 
 <script>
    document.addEventListener('DOMContentLoaded', function() {
+
         Echo.private('notify.{{ auth()->user()->id }}').listen('RegistrationNotificationEvent', (e) => {
-            toastr.success(e.data.name + ' ' + e.data.message);
+            toastr.success(e.data.name + ' ' + e.data.body);
         });
+
+        Echo.private('test-notify.{{ auth()->user()->id }}').listen('TestNotificationEvent', (e) => {
+            toastr.success(e.data.title + ' ' + e.data.body);
+        });
+
     });
 </script>
