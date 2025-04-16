17d6b92f1b663db407ed1c4129c480d780a27a19
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-04-16 20:22:35 +0600

    u

--- app/Http/Controllers/Api/Auth/LogoutController.php
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Helpers\Helper;
+use App\Models\FirebaseTokens;
 use Exception;
 
 class LogoutController extends Controller
@@ -18,7 +19,14 @@ class LogoutController extends Controller
     {
         try {
             if (Auth::check('api')) {
+
+                $firebaseTokens = FirebaseTokens::where('user_id', Auth::guard('api')->id())->get();
+                if ($firebaseTokens) {
+                    $firebaseTokens->each->delete();
+                }
+
                 Auth::logout('api');
+                
                 return Helper::jsonResponse(true, 'Logged out successfully. Token revoked.', 200);
             } else {
                 return Helper::jsonErrorResponse( 'User not authenticated', 401);
