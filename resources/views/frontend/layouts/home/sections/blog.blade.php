cfb73a43f3de2d821f76532a429143287bbfb91c
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-06-11 17:29:37 +0600

    u

--- resources/views/frontend/layouts/home/sections/blog.blade.php
+++ resources/views/frontend/layouts/home/sections/blog.blade.php
@@ -9,25 +9,27 @@
 
         <div class="row blog-wrapper">
 
-        @if(isset($posts) && $posts->count() > 0)
-            @foreach($posts as $post)
+            @forelse($posts as $post)
             <div class="col-md-4">
                 <div class="blog-item rounded bg-white shadow-dark wow fadeIn">
                     <div class="thumb">
-                        <a href="#"><span class="category">Reviews</span></a>
-                        <a href="#"><img src="{{ asset('default/logo.svg') }}" alt="blog-title" style="width:330px; height:215px;" /></a>
+                        <a href="#"><span class="category">{{ $post->category->name }}</span></a>
+                        <a href="#"><img src="{{ asset($post->thumbnail ?? 'default/logo.svg') }}" alt="blog-title" style="width:330px; height:215px;" /></a>
                     </div>
                     <div class="details">
-                        <h4 class="my-0 title"><a href="#">5 Best App Development Tool for Your Project</a></h4>
+                        <h4 class="my-0 title"><a href="#">{{ $post->title ?? 'Unnamed Project' }}</a></h4>
                         <ul class="list-inline meta mb-0 mt-2">
-                            <li class="list-inline-item">09 February, 2020</li>
-                            <li class="list-inline-item">Bolby</li>
+                            <li class="list-inline-item">{{ $post->created_at->format('M d, Y') }}</li>
+                            <li class="list-inline-item">{{ $post->user->name }}</li>
                         </ul>
                     </div>
                 </div>
             </div>
-            @endforeach
-        @else
+            @empty
+            <div class="col-12 text-center">
+                <p class="text-muted">No posts available.</p>
+            </div>
+            @endforelse
 
         </div>
 
