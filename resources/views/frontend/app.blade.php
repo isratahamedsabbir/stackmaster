a908d642c6acfd056cd37744437c01fbabd2da73
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-06-12 12:30:27 +0600

    u

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -22,6 +22,8 @@
 
   <meta name="twitter:card" content="summary" />
   <meta name="twitter:image" content="{{ $image ?? asset(settings()->thumbnail ?? 'default/logo.png') }}" />
+  <meta name="twitter:title" content="{{ $title ?? settings()->title ?? config('app.name') }}" />
+  <meta name="twitter:description" content="{!! strip_tags($description ?? settings()->description ?? '') !!}" />
 
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
