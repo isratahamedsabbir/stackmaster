55fb735cb36233bf70a7522dfad777212ef8da97
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-06-12 17:21:27 +0600

    u

--- resources/views/frontend/layouts/home/sections/intro.blade.php
+++ resources/views/frontend/layouts/home/sections/intro.blade.php
@@ -5,7 +5,7 @@
         <!-- intro -->
         <div class="intro">
             <!-- avatar image -->
-            <img src="{{ asset(!empty($cms_intro->image) && file_exists(public_path($cms_intro->image)) ? $cms_intro->image : 'default/logo.svg') }}" alt="Bolby" class="mb-4" style="width: 108px; height: 108px" />
+            <img src="{{ asset(!empty($cms_intro->image) && file_exists(public_path($cms_intro->image)) ? $cms_intro->image : 'default/logo.svg') }}" alt="Bolby" class="mb-4"/>
 
             <!-- info -->
             <h1 class="mb-2 mt-0">{{ settings()->author ?? "Israt Ahamed Sabbir" }}</h1>
