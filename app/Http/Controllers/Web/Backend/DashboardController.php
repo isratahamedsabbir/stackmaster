c36d90213d0c6ad5eff1a566776dfebca02c4dd5
tanimahasan786 <tanimahasan786@gmail.com>
2025-07-11 14:25:19 +0600

    u

--- app/Http/Controllers/Web/Backend/DashboardController.php
+++ app/Http/Controllers/Web/Backend/DashboardController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\DB;
 
+use function App\Helpers\parseTemplate;
+
 class DashboardController extends Controller
 {
     public function index()
