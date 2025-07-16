cfdc0e7882a9f0e8dd1fce97c90db8bafd4cbfc6
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-07-16 18:49:16 +0600

    u

--- resources/views/backend/layouts/menu/index.blade.php
+++ resources/views/backend/layouts/menu/index.blade.php
@@ -30,37 +30,71 @@
 
             <!-- ROW-4 -->
             <div class="row">
-                <div class="col-12 col-sm-12">
+                <div class="col-6 col-sm-6">
                     <div class="card product-sales-main">
                         <div class="card-header border-bottom">
                             <h3 class="card-title mb-0">List</h3>
-                            <div class="card-options ms-auto">
-                                <a href="{{ route('admin.menu.create') }}" class="btn btn-primary btn-sm">Add</a>
-                            </div>
                         </div>
                         <div class="card-body">
-                            <div class="">
-                                <table class="table table-bordered text-nowrap border-bottom" id="datatable">
-                                    <thead>
-                                        <tr>
-                                            <th class="bg-transparent border-bottom-0 wp-15">ID</th>
-                                            <th class="bg-transparent border-bottom-0 wp-15">Title</th>
-                                            <th class="bg-transparent border-bottom-0 wp-15">Category</th>
-                                            <th class="bg-transparent border-bottom-0 wp-15">Subcategory</th>
-                                            <th class="bg-transparent border-bottom-0 wp-15">Author</th>
-                                            <th class="bg-transparent border-bottom-0">Thumbnail</th>
-                                            <th class="bg-transparent border-bottom-0">Status</th>
-                                            <th class="bg-transparent border-bottom-0">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
 
+                            @if($menus->count() > 0)
+                                <ul class="list-group">
+                                    @foreach($menus as $menu)
+                                        <li class="list-group-item d-flex justify-content-between align-items-center mb-3">
+                                            <div>
+                                                <h5 class="mb-0">{{ $menu->name }}</h5>
+                                                <p class="text-muted mb-0">{{ $menu->description }}</p>
+                                            </div>
+                                            <div>
+                                                <a href="{{ route('admin.menu.edit', $menu->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                                                <form action="{{ route('admin.menu.destroy', $menu->id) }}" method="POST" class="d-inline-block">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                                </form>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            @else
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <div>
+                                        <h5 class="mb-0">No data found</h5>
+                                    </div>
+                                </div>
+                            @endif
+
+                        </div>
                     </div>
                 </div><!-- COL END -->
+
+                <div class="col-md-6 col-sm-6">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between border-bottom">
+                            <h3 class="card-title">Content Edit</h3>
+                        </div>
+                        <div class="card-body">
+                            <form class="form form-horizontal" method="POST" action="{{ route('admin.post.store') }}" enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <x-form.text name="title" label="Title" placeholder="Enter here title" :value="old('title') ?? ''" />
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <div class="col-md-12">
+                                        <button class="submit btn btn-primary" type="submit">Submit</button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <!-- ROW-4 END -->
 
@@ -73,181 +107,6 @@
 
 
 @push('scripts')
-<script>
-    $(document).ready(function() {
-
-        $.ajaxSetup({
-            headers: {
-                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            }
-        });
-        if (!$.fn.DataTable.isDataTable('#datatable')) {
-            let dTable = $('#datatable').DataTable({
-                order: [],
-                lengthMenu: [
-                    [10, 25, 50, 100, -1],
-                    [10, 25, 50, 100, "All"]
-                ],
-                processing: true,
-                responsive: true,
-                serverSide: true,
-
-                language: {
-                    processing: `<div class="text-center">
-                        <img src="{{ asset('default/loader.gif') }}" alt="Loader" style="width: 50px;">
-                        </div>`
-                },
-
-                scroller: {
-                    loadingIndicator: false
-                },
-                pagingType: "full_numbers",
-                dom: "<'row justify-content-between table-topbar'<'col-md-4 col-sm-3'l><'col-md-5 col-sm-5 px-0'f>>tipr",
-                ajax: {
-                    url: "{{ route('admin.menu.index') }}",
-                    type: "GET",
-                },
-
-                columns: [{
-                        data: 'DT_RowIndex',
-                        name: 'DT_RowIndex',
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: 'title',
-                        name: 'title',
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: 'category',
-                        name: 'category',
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: 'subcategory',
-                        name: 'subcategory',
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: 'author',
-                        name: 'author',
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: 'thumbnail',
-                        name: 'thumbnail',
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: 'status',
-                        name: 'status',
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: 'action',
-                        name: 'action',
-                        orderable: false,
-                        searchable: false,
-                        className: 'dt-center text-center'
-                    },
-                ],
-            });
-        }
-    });
-
-    // Status Change Confirm Alert
-    function showStatusChangeAlert(id) {
-        event.preventDefault();
-
-        Swal.fire({
-            title: 'Are you sure?',
-            text: 'You want to update the status?',
-            icon: 'info',
-            showCancelButton: true,
-            confirmButtonText: 'Yes',
-            cancelButtonText: 'No',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                statusChange(id);
-            }
-        });
-    }
-
-    // Status Change
-    function statusChange(id) {
-        NProgress.start();
-        let url = "{{ route('admin.menu.status', ':id') }}";
-        $.ajax({
-            type: "GET",
-            url: url.replace(':id', id),
-            success: function(resp) {
-                NProgress.done();
-                toastr.success(resp.message);
-                $('#datatable').DataTable().ajax.reload();
-            },
-            error: function(error) {
-                NProgress.done();
-                toastr.error(error.message);
-            }
-        });
-    }
-
-    // delete Confirm
-    function showDeleteConfirm(id) {
-        event.preventDefault();
-        Swal.fire({
-            title: 'Are you sure you want to delete this record?',
-            text: 'If you delete this, it will be gone forever.',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                deleteItem(id);
-            }
-        });
-    }
-
-    // Delete Button
-    function deleteItem(id) {
-        NProgress.start();
-        let url = "{{ route('admin.menu.destroy', ':id') }}";
-        let csrfToken = '{{ csrf_token() }}';
-        $.ajax({
-            type: "DELETE",
-            url: url.replace(':id', id),
-            headers: {
-                'X-CSRF-TOKEN': csrfToken
-            },
-            success: function(resp) {
-                NProgress.done();
-                toastr.success(resp.message);
-                $('#datatable').DataTable().ajax.reload();
-            },
-            error: function(error) {
-                NProgress.done();
-                toastr.error(error.message);
-            }
-        });
-    }
-
-    //edit
-    function goToEdit(id) {
-        let url = "{{ route('admin.menu.edit', ':id') }}";
-        window.location.href = url.replace(':id', id);
-    }
-    function goToOpen(id) {
-        let url = "{{ route('admin.menu.show', ':id') }}";
-        window.location.href = url.replace(':id', id);
-    }
-</script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 @endpush
