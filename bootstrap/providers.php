f4144a998c5af20d75de2022d3c1731f783c0fd3
isratahamedsabbir <93650049+isratahamedsabbir@users.noreply.github.com>
2025-09-30 17:53:42 +0600

    feat: add CustomServiceProvider to enhance service registration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\CustomPrivider::class,
     App\Providers\RepositoryServiceProvider::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
 ];
