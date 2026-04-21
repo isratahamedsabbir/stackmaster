23541ed9b459db022a646a00176d39f770c82fdc
isratahamedsabbir <93650049+isratahamedsabbir@users.noreply.github.com>
2026-04-21 14:23:32 +0600

    u

---
+++ app/Http/Controllers/Api/V2/Gateway/PaymentStatusController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2\Gateway;
+
+use App\Http\Controllers\Controller;
+
+class PaymentStatusController extends Controller
+{
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -22,183 +22,189 @@ use App\Http\Controllers\Api\V1\Frontend\SubscriberController;
 use App\Http\Controllers\Api\V1\MCPController;
 use App\Http\Controllers\Api\V1\NotificationController;
 use App\Http\Controllers\Api\V1\PrayerTimesController;
+use App\Http\Controllers\Api\V2\Gateway\PaymentStatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
-Route::get('/page/home', [HomeController::class, 'index']);
+Route::prefix('v1')->name('v1')->group(function () {
 
-Route::get('/category', [categoryController::class, 'index']);
-Route::get('/subcategory', [SubcategoryController::class, 'index']);
+    Route::get('/page/home', [HomeController::class, 'index']);
 
-Route::get('/social/links', [SocialLinksController::class, 'index']);
-Route::get('/settings', [SettingsController::class, 'index']);
-Route::get('/faq', [FaqController::class, 'index']);
+    Route::get('/category', [categoryController::class, 'index']);
+    Route::get('/subcategory', [SubcategoryController::class, 'index']);
 
-Route::post('subscriber/store', [SubscriberController::class, 'store'])->name('api.subscriber.store');
+    Route::get('/social/links', [SocialLinksController::class, 'index']);
+    Route::get('/settings', [SettingsController::class, 'index']);
+    Route::get('/faq', [FaqController::class, 'index']);
 
-/*
-# Post
-*/
-Route::middleware(['auth:api'])->controller(PostController::class)->prefix('auth/post')->group(function () {
-    Route::get('/', 'index');
-    Route::post('/store', 'store');
-    Route::get('/show/{id}', 'show');
-    Route::post('/update/{id}', 'update');
-    Route::delete('/delete/{id}', 'destroy');
-});
+    Route::post('subscriber/store', [SubscriberController::class, 'store'])->name('api.subscriber.store');
 
-Route::get('/posts', [PostController::class, 'posts']);
-Route::get('/post/show/{post_id}', [PostController::class, 'post']);
+    /*
+    # Post
+    */
+    Route::middleware(['auth:api'])->controller(PostController::class)->prefix('auth/post')->group(function () {
+        Route::get('/', 'index');
+        Route::post('/store', 'store');
+        Route::get('/show/{id}', 'show');
+        Route::post('/update/{id}', 'update');
+        Route::delete('/delete/{id}', 'destroy');
+    });
 
-Route::middleware(['auth:api'])->controller(ImageController::class)->prefix('auth/post/image')->group(function () {
-    Route::get('/', 'index');
-    Route::post('/store', 'store');
-    Route::get('/delete/{id}', 'destroy');
-});
+    Route::get('/posts', [PostController::class, 'posts']);
+    Route::get('/post/show/{post_id}', [PostController::class, 'post']);
 
-Route::get('dynamic/page', [PageController::class, 'index']);
-Route::get('dynamic/page/show/{slug}', [PageController::class, 'show']);
+    Route::middleware(['auth:api'])->controller(ImageController::class)->prefix('auth/post/image')->group(function () {
+        Route::get('/', 'index');
+        Route::post('/store', 'store');
+        Route::get('/delete/{id}', 'destroy');
+    });
 
-/*
-# Auth Route
-*/
+    Route::get('dynamic/page', [PageController::class, 'index']);
+    Route::get('dynamic/page/show/{slug}', [PageController::class, 'show']);
 
-Route::group(['middleware' => 'guest:api'], function ($router) {
-    // register
-    Route::post('register', [RegisterController::class, 'register']);
-    Route::post('/verify-email', [RegisterController::class, 'VerifyEmail']);
-    Route::post('/resend-otp', [RegisterController::class, 'ResendOtp']);
-    Route::post('/verify-otp', [RegisterController::class, 'VerifyEmail']);
-    // login
-    Route::post('login', [LoginController::class, 'login'])->name('api.login');
-    // forgot password
-    Route::post('/forget-password', [ResetPasswordController::class, 'forgotPassword']);
-    Route::post('/otp-token', [ResetPasswordController::class, 'MakeOtpToken']);
-    Route::post('/reset-password', [ResetPasswordController::class, 'ResetPassword']);
-    // social login
-    Route::post('/social-login', [SocialLoginController::class, 'SocialLogin']);
-});
+    /*
+    # Auth Route
+    */
 
-Route::group(['middleware' => ['auth:api', 'api-otp']], function ($router) {
-    Route::get('/refresh-token', [LoginController::class, 'refreshToken']);
-    Route::post('/logout', [LogoutController::class, 'logout']);
-    Route::get('/me', [UserController::class, 'me']);
-    Route::get('/account/switch', [UserController::class, 'accountSwitch']);
-    Route::post('/update-profile', [UserController::class, 'updateProfile']);
-    Route::post('/update-avatar', [UserController::class, 'updateAvatar']);
-    Route::delete('/delete-profile', [UserController::class, 'destroy']);
-});
+    Route::group(['middleware' => 'guest:api'], function ($router) {
+        // register
+        Route::post('register', [RegisterController::class, 'register']);
+        Route::post('/verify-email', [RegisterController::class, 'VerifyEmail']);
+        Route::post('/resend-otp', [RegisterController::class, 'ResendOtp']);
+        Route::post('/verify-otp', [RegisterController::class, 'VerifyEmail']);
+        // login
+        Route::post('login', [LoginController::class, 'login'])->name('api.login');
+        // forgot password
+        Route::post('/forget-password', [ResetPasswordController::class, 'forgotPassword']);
+        Route::post('/otp-token', [ResetPasswordController::class, 'MakeOtpToken']);
+        Route::post('/reset-password', [ResetPasswordController::class, 'ResetPassword']);
+        // social login
+        Route::post('/social-login', [SocialLoginController::class, 'SocialLogin']);
+    });
 
-/*
-# Firebase Notification Route
-*/
+    Route::group(['middleware' => ['auth:api', 'api-otp']], function ($router) {
+        Route::get('/refresh-token', [LoginController::class, 'refreshToken']);
+        Route::post('/logout', [LogoutController::class, 'logout']);
+        Route::get('/me', [UserController::class, 'me']);
+        Route::get('/account/switch', [UserController::class, 'accountSwitch']);
+        Route::post('/update-profile', [UserController::class, 'updateProfile']);
+        Route::post('/update-avatar', [UserController::class, 'updateAvatar']);
+        Route::delete('/delete-profile', [UserController::class, 'destroy']);
+    });
 
-Route::middleware(['auth:api'])->controller(FirebaseTokenController::class)->prefix('firebase')->group(function () {
-    Route::get('test', 'test');
-    Route::post('token/add', 'store');
-    Route::post('token/get', 'getToken');
-    Route::post('token/delete', 'deleteToken');
-});
+    /*
+    # Firebase Notification Route
+    */
 
-/*
-# In App Notification Route
-*/
+    Route::middleware(['auth:api'])->controller(FirebaseTokenController::class)->prefix('firebase')->group(function () {
+        Route::get('test', 'test');
+        Route::post('token/add', 'store');
+        Route::post('token/get', 'getToken');
+        Route::post('token/delete', 'deleteToken');
+    });
 
-Route::middleware(['auth:api'])->controller(NotificationController::class)->prefix('notify')->group(function () {
-    Route::get('test', 'test');
-    Route::get('/', 'index');
-    Route::get('read/{id?}', 'read');
-});
+    /*
+    # In App Notification Route
+    */
 
-/*
-# Chat Route
-*/
+    Route::middleware(['auth:api'])->controller(NotificationController::class)->prefix('notify')->group(function () {
+        Route::get('test', 'test');
+        Route::get('/', 'index');
+        Route::get('read/{id?}', 'read');
+    });
 
-Route::middleware(['auth:api'])->controller(ChatController::class)->prefix('auth/chat')->group(function () {
-    Route::get('/list', 'list');
-    Route::post('/send/{receiver_id}', 'send');
-    Route::get('/conversation/{receiver_id}', 'conversation');
-    Route::get('/room/{receiver_id}', 'room');
-    Route::get('/search', 'search');
-    Route::get('/seen/all/{receiver_id}', 'seenAll');
-    Route::get('/seen/single/{chat_id}', 'seenSingle');
-});
+    /*
+    # Chat Route
+    */
 
-/*
-# CMS
-*/
+    Route::middleware(['auth:api'])->controller(ChatController::class)->prefix('auth/chat')->group(function () {
+        Route::get('/list', 'list');
+        Route::post('/send/{receiver_id}', 'send');
+        Route::get('/conversation/{receiver_id}', 'conversation');
+        Route::get('/room/{receiver_id}', 'room');
+        Route::get('/search', 'search');
+        Route::get('/seen/all/{receiver_id}', 'seenAll');
+        Route::get('/seen/single/{chat_id}', 'seenSingle');
+    });
 
-Route::prefix('cms')->name('cms.')->group(function () {
-    Route::get('home', [HomeController::class, 'index'])->name('home');
-});
+    /*
+    # CMS
+    */
 
-/*
-# prayer time
-# http:://127.0.0.1:8000/api/prayer-times?date=2025-12-25&lat=23.7018&lng=90.3742&timezone=6&method=1
-# http:://127.0.0.1:8000/api/prayer-times/today?lat=23.7018&lng=90.3742&timezone=6&method=1
-*/
-Route::prefix('prayer-times')->group(function () {
-    Route::get('/', [PrayerTimesController::class, 'index']);
-    Route::get('/today', [PrayerTimesController::class, 'today']);
-    Route::get('/methods', [PrayerTimesController::class, 'methods']);
-});
+    Route::prefix('cms')->name('cms.')->group(function () {
+        Route::get('home', [HomeController::class, 'index'])->name('home');
+    });
 
-Route::post('contact/store', [ContactController::class, 'store'])->name('contact.store');
+    /*
+    # prayer time
+    # http:://127.0.0.1:8000/api/prayer-times?date=2025-12-25&lat=23.7018&lng=90.3742&timezone=6&method=1
+    # http:://127.0.0.1:8000/api/prayer-times/today?lat=23.7018&lng=90.3742&timezone=6&method=1
+    */
+    Route::prefix('prayer-times')->group(function () {
+        Route::get('/', [PrayerTimesController::class, 'index']);
+        Route::get('/today', [PrayerTimesController::class, 'today']);
+        Route::get('/methods', [PrayerTimesController::class, 'methods']);
+    });
+
+    Route::post('contact/store', [ContactController::class, 'store'])->name('contact.store');
 
 
-/*
-# test code
-*/
-Route::get('/users', [UserController::class, 'users']);
+    /*
+    # test code
+    */
+    Route::get('/users', [UserController::class, 'users']);
 
-Route::get('telegram/messages', function () {
+    Route::get('telegram/messages', function () {
 
-    $token = config('services.telegram.token');
-    $chatId = config('services.telegram.channel');
+        $token = config('services.telegram.token');
+        $chatId = config('services.telegram.channel');
 
-    $url = "https://api.telegram.org/bot{$token}/sendMessage";
+        $url = "https://api.telegram.org/bot{$token}/sendMessage";
 
-    Http::post($url, [
-        'chat_id' => $chatId,
-        'text' => "hello from laravel 123",
-        'parse_mode' => 'HTML'
-    ]);
-});
+        Http::post($url, [
+            'chat_id' => $chatId,
+            'text' => "hello from laravel 123",
+            'parse_mode' => 'HTML'
+        ]);
+    });
 
-Route::get('/user-by-name', function (Request $request) {
-    $name = $request->input('name');
+    Route::get('/user-by-name', function (Request $request) {
+        $name = $request->input('name');
 
-    return \App\Models\User::where('name', 'LIKE', "%$name%")
-        ->select('name', 'email')
-        ->get();
-});
+        return \App\Models\User::where('name', 'LIKE', "%$name%")
+            ->select('name', 'email')
+            ->get();
+    });
 
-Route::get('/user-email', function (Request $request) {
+    Route::get('/user-email', function (Request $request) {
 
-    $name = $request->input('name');
+        $name = $request->input('name');
 
-    $users = \App\Models\User::where('name', 'LIKE', "%$name%")
-        ->select('name', 'email')
-        ->get();
+        $users = \App\Models\User::where('name', 'LIKE', "%$name%")
+            ->select('name', 'email')
+            ->get();
 
-    // Gemini API call
-    $response = Http::post('https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=gen-lang-client-0194670477', [
-        "contents" => [
-            [
-                "parts" => [
-                    [
-                        "text" => "User data: " . $users->toJson() . ". 
-                        Give a clean response with name and email."
+        // Gemini API call
+        $response = Http::post('https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=gen-lang-client-0194670477', [
+            "contents" => [
+                [
+                    "parts" => [
+                        [
+                            "text" => "User data: " . $users->toJson() . ". 
+                            Give a clean response with name and email."
+                        ]
                     ]
                 ]
             ]
-        ]
-    ]);
+        ]);
 
-    return $response->json();
+        return $response->json();
+    });
+    
 });
 
-Route::prefix('api/v1')->name('api.v1')->group(function () {
-    
+Route::prefix('v2')->name('v2')->group(function () {
+    Route::get('/payment/success', [PaymentStatusController::class, 'success']);
+    Route::get('/payment/cancel', [PaymentStatusController::class, 'cancel']);
 });
